9ed6431d644ce76275e22e244c3188e222a3cee8
Jonah Spear <Jonahspear512@gmail.com>
2017-11-28 12:02:26 -0500

    Remove pep8 linting issues

--- classes/eqNodes.py
+++ classes/eqNodes.py
@@ -1,28 +1,33 @@
 class VariableNode:
-	def __init__(self, name):
-		self.name = name.lower()
-	def eval(self, lookup):
-		return lookup[self.name]
+    def __init__(self, name):
+        self.name = name.lower()
+
+    def eval(self, lookup):
+        return lookup[self.name]
+
 
 class UnaryNode:
-	def __init__(self, fn, name, arg1 = None):
-		self.name = name.lower()
-		self.fn = fn
-		self.arg1 = arg1
-	def eval(self, lookup):
-		if (arg1 == None):
-			raise ValueError("Arg1 should not be None")
-		return self.fn(arg1.eval(lookup))
+    def __init__(self, fn, name, arg1=None):
+        self.name = name.lower()
+        self.fn = fn
+        self.arg1 = arg1
+
+    def eval(self, lookup):
+        if (arg1 is None):
+            raise ValueError("Arg1 should not be None")
+        return self.fn(arg1.eval(lookup))
+
 
 class BinaryNode:
-	def __init__(self, fn, name, arg1 = None, arg2 = None):
-		self.name = name.lower()
-		self.fn = fn
-		self.arg1 = arg1
-		self.arg2 = arg2
-	def eval(self, lookup):
-		if (arg1 == None):
-			raise ValueError("Arg1 should not be None")
-		if (arg2 == None):
-			raise ValueError("Arg2 should not be None")
-		return self.fn(arg1.eval(lookup), arg2.eval(lookup))
+    def __init__(self, fn, name, arg1=None, arg2=None):
+        self.name = name.lower()
+        self.fn = fn
+        self.arg1 = arg1
+        self.arg2 = arg2
+
+    def eval(self, lookup):
+        if (arg1 is None):
+            raise ValueError("Arg1 should not be None")
+        if (arg2 is None):
+            raise ValueError("Arg2 should not be None")
+        return self.fn(arg1.eval(lookup), arg2.eval(lookup))
