c8071885eab4ceb971151e4e6b84092cfee807df
apan64 <andrew.pan@students.olin.edu>
2017-12-10 14:13:55 -0500

    minor changes to synthesize.py

--- src/synthesize.py
+++ src/synthesize.py
@@ -3,5 +3,5 @@ def synthesizeSingleOutput(minTerms):
 	Input: list of minTerms
 	Output: Head of a tree
 	'''
-	primeImplicants = findPrimeImplicants(minTerms)
-	return tabulation(primeImplicants)
+	primeImplicants = findPrimeImplicantsSingleOutput(minTerms)
+	return tabulationSingleOutput(primeImplicants, minTerms)
